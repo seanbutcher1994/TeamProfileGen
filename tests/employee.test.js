9e8149b367f42db9db6c495923ab557c70188f17
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2022-10-23 17:38:13 +0800

    add test from employee js file

---
+++ tests/employee.test.js
@@ -0,0 +1,30 @@
+// Get access to Employee JS file
+const Employee = require('../lib/Employee');
+// create new employee with an Id, email and name property
+const employee = new Employee('123', 'sean@sean.com', 'sean')
+
+// Runs tests to confirm that the functions return the correct values
+test('can we get the employee instance to instantiate', () => {
+    expect(employee.name).toBe('sean');
+    expect(employee.id).toBe('123');
+    expect(employee.email).toBe('sean@sean.com');
+});
+
+test('getID should give the correct ID', () => {
+    const actualID = '123';
+    expect(employee.getId()).toBe(actualID);
+});
+
+test('getEmail should return the correct email', () => {
+    const actualEmail = 'sean@sean.com'
+    expect(employee.getEmail()).toBe(actualEmail);
+});
+
+test('getName should return the correct name', () => {
+    const actualName = 'sean';
+    expect(employee.getName()).toBe(actualName);
+})
+test('getRole should return the correct role', () => {
+    const actualRole = 'Employee';
+    expect(employee.getRole()).toBe(actualRole);
+})
